76527ad87e3de74f46937ca71e58e65e057637e5
Nicholas Kwon <nichkwon@gmail.com>
2016-02-02 01:07:10 -0600

    init commit

---
+++ app/src/main/java/com/nichhk/remotestream/ConfigActivity.java
@@ -0,0 +1,18 @@
+package com.nichhk.remotestream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sveder.remotestream.R;
+
+/**
+ * Created by nich on 1/23/16.
+ */
+public class ConfigActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config);
+    }
+}
---
+++ app/src/main/java/com/nichhk/remotestream/mjpeg/MjpegPlayer.java
@@ -0,0 +1,135 @@
+package com.nichhk.remotestream.mjpeg;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.nichhk.remotestream.CardboardOverlayView;
+import com.nichhk.remotestream.mjpeg.MjpegInputStream;
+
+import java.io.IOException;
+
+public class MjpegPlayer implements SurfaceHolder.Callback{
+
+    public final static int POSITION_LOWER_RIGHT = 6;
+
+    public final static int SIZE_STANDARD   = 1;
+    public final static int SIZE_BEST_FIT   = 4;
+    public final static int SIZE_FULLSCREEN = 8;
+
+    private MjpegViewThread thread;
+    private MjpegInputStream mIn = null;
+    private boolean showFps = false;
+    private boolean mRun = false;
+    private Paint overlayPaint;
+
+    private int dispWidth;
+    private int dispHeight;
+    private int displayMode;
+
+
+    private boolean surface1Done, surface2Done;
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.i("TAG", "a surface was created!");
+        if (surface1Done){
+            surface2Done = true;
+        }
+        else{
+            surface1Done = true;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    public class MjpegViewThread extends Thread {
+        private SurfaceView[] surfaces;
+
+        public MjpegViewThread(SurfaceView ... surfaces) { this.surfaces = surfaces; }
+
+        public void run() {
+
+            final Paint p = new Paint();
+            while (mRun) {
+                if (surface1Done && surface2Done) {
+                    try {
+                        final Bitmap bm = mIn.readMjpegFrame();
+                        Bitmap scaled = Bitmap.createScaledBitmap(bm, surfaces[0].getWidth(), surfaces[0].getHeight(), false);
+                        for (final SurfaceView surfaceView : surfaces) {
+                            SurfaceHolder surface = surfaceView.getHolder();
+                            synchronized (surface) {
+                                Canvas c = surface.lockCanvas();
+                                c.drawColor(Color.BLACK);
+                                c.drawBitmap(scaled, 0, 0, p);
+                                surface.unlockCanvasAndPost(c);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    private void init(int width, int height, SurfaceView ... holders) {
+        thread = new MjpegViewThread(holders);
+        overlayPaint = new Paint();
+        overlayPaint.setTextAlign(Paint.Align.LEFT);
+        overlayPaint.setTextSize(12);
+        overlayPaint.setTypeface(Typeface.DEFAULT);
+        displayMode = MjpegPlayer.SIZE_STANDARD;
+        dispWidth = width;
+        dispHeight = height;
+    }
+
+    public void startPlayback() {
+        if(mIn != null) {
+            mRun = true;
+            thread.start();
+        }
+    }
+
+    public void stopPlayback() {
+        mRun = false;
+        boolean retry = true;
+        while(retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {}
+        }
+    }
+
+
+    public MjpegPlayer(int w, int h, CardboardOverlayView cov) {
+        init(w, h, cov.getSurfaceViews());
+        cov.setCallback(this);
+    }
+
+
+    public void setSource(MjpegInputStream source) {
+        mIn = source;
+        startPlayback();
+    }
+
+    public void setDisplayMode(int s) {
+        displayMode = s;
+    }
+}
