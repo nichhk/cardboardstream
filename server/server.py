efe8b75d2de6342845fb3558f5fe48e16e31906e
Nicholas Kwon <nichkwon@gmail.com>
2016-03-05 14:42:40 -0600

    improved style

--- server/server.py
+++ server/server.py
@@ -7,30 +7,39 @@ app = Flask(__name__)
 
 pwm = PWM(0x40)
 
-servoMin = 150
-servoMax = 600
-half_pi = math.pi / 2.0
-midpoint = 375.0
-span = 225.0
+# Constants defining the range of the servo
+SERVO_MIN = 150
+SERVO_MAX = 600
+HALF_PI = math.pi / 2.0
+MIDPOINT = 375.0
+SPAN = 225.0
+
+# channels
+PAN = 3
+TILT = 0
 
 def init():
     pwm.setPWMFreq(60)
-    pwm.setPWM(0, 0, 375)
-    pwm.setPWM(0, 0, 375)
+    pwm.setPWM(PAN, 0, 375)
+    pwm.setPWM(TILT, 0, 375)
 
 
 @app.route('/')
 def hello():
-    return "sawP:"
+    return "hello"
+
 
 def normalize(num):
-    return int(midpoint + ((num / half_pi) * span))
+    '''
+    num: a float between -PI and PI.
+    returns: num normalized between SERVO_MIN and SERVO_MAX.
+    '''
+    return int(MIDPOINT + ((num / HALF_PI) * SPAN))
 
 @app.route("/move/<pitch>/<yaw>")
 def move(pitch, yaw):
-    pwm.setPWM(0, 0, normalize(-float(pitch)))
-    pwm.setPWM(3, 0, normalize(float(yaw)))
-    print "moved the motors"
+    pwm.setPWM(TILT, 0, normalize(-float(pitch)))
+    pwm.setPWM(PAN, 0, normalize(float(yaw)))
     return "OK"
 
 if __name__ == '__main__':
